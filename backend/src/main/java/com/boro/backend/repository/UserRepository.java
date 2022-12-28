f7eea460428ad342560146e48274674a546b3dac
plgborowiczm <mikolajborowicz7@gmail.com>
2022-12-28 16:44:43 +0100

    cos tam dziala

--- backend/src/main/java/com/boro/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.boro.backend.repository;
-
-import com.boro.backend.model.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
---
+++ backend/src/main/java/com/example/backendcatering/controller/CourseController.java
@@ -0,0 +1,2 @@
+package com.example.backendcatering.controller;public class CourseController {
+}
