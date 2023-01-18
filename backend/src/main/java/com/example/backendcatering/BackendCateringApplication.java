cf6b21a9bbfb056690ef98b77c24a51d4211fccc
plgborowiczm <mikolajborowicz7@gmail.com>
2023-01-18 23:23:45 +0100

    dodanie koszyka

--- backend/src/main/java/com/example/backendcatering/BackendCateringApplication.java
+++ backend/src/main/java/com/example/backendcatering/BackendCateringApplication.java
@@ -1,12 +1,10 @@
 package com.example.backendcatering;
 
-import com.example.backendcatering.configuration.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RsaKeyProperties.class)
 public class BackendCateringApplication {
 
     public static void main(String[] args) {
